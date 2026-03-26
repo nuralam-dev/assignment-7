ac86b9130dfa264f31b0e8d514b05fa8678694e6
Nur Alam <nuralam1122345@gmail.com>
2026-03-26 15:52:56 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>
-          helloooo
-        </h1>
-      </div>
+     <h1>hello</h1>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // এই লাইনটি যেন অবশ্যই থাকে
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // এখানে প্লাগিনটি কল করা হয়েছে
+  ],
 })
